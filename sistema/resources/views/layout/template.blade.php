f3a1d44f87203b952a39a9bf187bd0726b3698a9
Carlos <dawcarlospereira@gmail.com>
2025-02-09 14:55:55 +0100

    tanteo

--- sistema/resources/views/layout/template.blade.php
+++ sistema/resources/views/layout/template.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/svg" href="{{asset('favicon.svg')}}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -18,11 +19,13 @@
         </div>
     </main>
     <footer class="text-end">
-        <img src="https://i.pinimg.com/originals/f0/2f/78/f02f786e4b205a91284f8fca6eb35c07.jpg" alt="" width="30px" height="auto" class="rounded-5">
-        <span>Guardián</span>
-        <img src="https://i.pinimg.com/originals/f0/2f/78/f02f786e4b205a91284f8fca6eb35c07.jpg" alt="" width="30px" height="auto" class="rounded-5">
-
+        <div class="dog">
+            <img src="https://i.pinimg.com/originals/f0/2f/78/f02f786e4b205a91284f8fca6eb35c07.jpg" alt="" width="50px" height="auto" class="rounded-5">
+            <span>Guardián</span>
+            <img src="https://i.pinimg.com/originals/f0/2f/78/f02f786e4b205a91284f8fca6eb35c07.jpg" alt="" width="50px" height="auto" class="rounded-5">
+        </div>
     </footer>
 </body>
-<script src= "{{asset('js/script.js')}}"></script>
+<script src="{{asset('js/script.js')}}"></script>
+
 </html>
--- sistema/resources/views/productos/show.blade.php
+++ sistema/resources/views/productos/show.blade.php
@@ -8,6 +8,6 @@
 <h2 class="text-center">Descripcion: {{$producto->descripcion}}</h2>
 <h2 class="text-center">Precio: {{$producto->precio}}</h2>
 <h2 class="text-center">Existencias: {{$producto->existencia}}</h2>
-<img src="https://cdn.worldvectorlogo.com/logos/real-madrid-c-f.svg" alt="" width="200px" height="auto" class="my-3 rounded-5">
+<img src="https://cdn.worldvectorlogo.com/logos/real-madrid-c-f.svg" alt="" width="200px" height="auto" class="my-3 rounded-5" id="equipo">
 <a class="btn btn-light font-weight-bold" href="{{url('/productos')}}">Volver a inicio</a>
 @endsection
